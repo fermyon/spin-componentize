fa94a0514814b27290eb22251ac5f618951e2810
Joel Dice <joel.dice@fermyon.com>
2023-03-20 10:56:21 -0600

    move spin-componentize to root of repo
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- build.rs
+++ build.rs
@@ -5,29 +5,29 @@ fn main() {
 
     let mut cmd = Command::new("cargo");
     cmd.arg("build")
-        .current_dir("../adapter")
+        .current_dir("adapter")
         .arg("--release")
         .arg("--target=wasm32-unknown-unknown")
         .env("CARGO_TARGET_DIR", &out_dir);
 
     let status = cmd.status().unwrap();
     assert!(status.success());
-    println!("cargo:rerun-if-changed=../adapter");
+    println!("cargo:rerun-if-changed=adapter");
 
     let mut cmd = Command::new("cargo");
     cmd.arg("build")
-        .current_dir("../rust-case")
+        .current_dir("rust-case")
         .arg("--release")
         .arg("--target=wasm32-wasi")
         .env("CARGO_TARGET_DIR", &out_dir);
 
     let status = cmd.status().unwrap();
     assert!(status.success());
-    println!("cargo:rerun-if-changed=../rust-case");
+    println!("cargo:rerun-if-changed=rust-case");
 
     let mut cmd = Command::new("tinygo");
     cmd.arg("build")
-        .current_dir("../go-case")
+        .current_dir("go-case")
         .arg("-target=wasi")
         .arg("-gc=leaking")
         .arg("-no-debug")
@@ -37,5 +37,5 @@ fn main() {
 
     let status = cmd.status().unwrap();
     assert!(status.success());
-    println!("cargo:rerun-if-changed=../go-case");
+    println!("cargo:rerun-if-changed=go-case");
 }
